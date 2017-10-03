0f2ba162d21c94f95544eb86d496f1ad0d631a0f
Larisa Kreismanis <lkreismanis@luc.edu>
2017-10-03 05:24:34 +0000

    Small error change

--- src/test/java/cs271/lab/list/TestList.java
+++ src/test/java/cs271/lab/list/TestList.java
@@ -100,7 +100,7 @@ public class TestList {
     // DONE fix the expected values in the assertions below
     assertEquals(6, list.size());
     assertEquals(1, list.indexOf(77));
-    assertEquals(5, list.lastIndexOf("77"));
+    assertEquals(3, list.lastIndexOf(77));
     assertEquals(4, list.get(2).intValue());
     assertEquals(77, list.get(3).intValue());
     list.remove(Integer.valueOf(5)); // what does this one do?
